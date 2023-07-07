aca0926b5611e26a86cadb06ea53848824ce09c5
Teza01x <110857514+teza01x@users.noreply.github.com>
2023-07-07 22:49:16 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,8 +1,6 @@
 
-# admin_id = '603247380'
 
-
-token = '6342988181:AAF4F13eIb0S44laPDuxYns3U1tj_0nWFzQ'
+token = ''
 data_base = 'database.db'
-api_key = 'AIzaSyAlZA-exPvz0mQpjGpCu3ITFjBP2lsdz1I'
-table_id = '1EN2K7em6qt3hzwSbWF85duisAn20KmRklOsdyn7AA4Y'
+api_key = ''
+table_id = ''
